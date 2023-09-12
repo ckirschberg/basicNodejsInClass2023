8b1cd95c360567b1787cdf48389bf735c33d321f
Christian Kirschberg <ckirschberg@gmail.com>
2023-09-12 11:06:22 +0200

    mongodb connect success

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
 const http = require('http');
+const { MongoClient, ServerApiVersion } = require("mongodb");
  
 const hostname = '127.0.0.1';
 const port = 3003;
 
-let uri = 'mongodb://localhost:27017';
+let uri = 'mongodb://127.0.0.1:27017';
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri,  {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+}
+);
+
+async function run() {
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+
 
 
  
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+
+  run().catch(console.dir);
+
   res.end('We are the best web class ever! 333333847youi32y43uiyou32y');
 });
  
